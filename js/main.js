a4fd6046380286162a2bbb15cd7ec1ab4c08b5f6
alex-zucchini <alekseyaleksandrov86@gmail.com>
2020-12-09 13:23:24 +0300

    innerHTML

--- js/main.js
+++ js/main.js
@@ -5,5 +5,32 @@ let catalog = document.querySelector(".hidden-menu")
 
 catalogBtn.onclick = function() {
     catalog.classList.toggle("shown")
-    catalog.classList.toggle("hidden-menu__shown")
+    
 }
+
+let catalogContain1 = document.querySelector(".hidden-menu__contains1")
+let catalogContain2 = document.querySelector(".hidden-menu__contains2")
+let catalogContent = document.querySelector(".hidden-menu__content")
+
+catalogContain1.onclick = function() {
+    catalogContent.innerHTML = `<ul class="hidden-menu__tens">
+    <li><a href="#">ТЭНы для водонагревателей</a></li>
+    <li><a href="#">ТЭНы для стиральных машин</a></li>
+    <li><a href="#">Воздушные ТЭНы</a></li>
+    <li><a href="#">Водяные ТЭНы</a></li>
+    <li><a href="#">ТЭНы для промышленных и бытовых плит</a></li>
+    <li><a href="#">ТЭНы бытовые</a></li>
+    <li><a href="#">ТЭНы для дистилляторов и кулеров</a></li>
+    <li><a href="#">ТЭНы для сауны</a></li>
+    <li><a href="#">Блоки ТЭН</a></li>
+    <li><a href="#">Нагревательные элементы для конвекторов</a></li>
+    <li><a href="#">Запчасти для ТЭНов</a></li>
+    </ul>`
+}
+
+
+
+
+
+
+
