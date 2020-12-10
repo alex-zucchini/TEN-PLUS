b445182a7b9ace61dc883c72761f723e3cecbd4e
alex-zucchini <alekseyaleksandrov86@gmail.com>
2020-12-10 16:02:40 +0300

    index page hidden menu fix

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 let catalogBtn = document.querySelector(".header__catalog")
 let catalog = document.querySelector(".hidden-menu")
 
-catalogBtn.onclick = function() {
+catalogBtn.onclick = function () {
     catalog.classList.toggle("shown")
-    
+
 }
 
 let catalogContain1 = document.querySelector(".hidden-menu__contains1")
@@ -16,7 +16,7 @@ let catalogContain5 = document.querySelector(".hidden-menu__contains5")
 let catalogContain6 = document.querySelector(".hidden-menu__contains6")
 let catalogContent = document.querySelector(".hidden-menu__content")
 
-catalogContain1.addEventListener("mouseover", function() { 
+catalogContain1.addEventListener("mouseover", function () {
     catalogContent.innerHTML = `<ul>
     <li><a href="#">ТЭНы для водонагревателей</a></li>
     <li><a href="#">ТЭНы для стиральных машин</a></li>
@@ -30,10 +30,15 @@ catalogContain1.addEventListener("mouseover", function() {
     <li><a href="#">Нагревательные элементы для конвекторов</a></li>
     <li><a href="#">Запчасти для ТЭНов</a></li>
     </ul>`
-}
-)
+    catalogContain2.classList.remove("chosen")
+    catalogContain3.classList.remove("chosen")
+    catalogContain4.classList.remove("chosen")
+    catalogContain5.classList.remove("chosen")
+    catalogContain6.classList.remove("chosen")
+    catalogContain1.classList.add("chosen")
+})
 
-catalogContain2.addEventListener("mouseover", function() { 
+catalogContain2.addEventListener("mouseover", function () {
     catalogContent.innerHTML = `<ul>
     <li><a href="#">ТЭНы</a></li>
     <li><a href="#">Помпы</a></li>
@@ -43,26 +48,41 @@ catalogContain2.addEventListener("mouseover", function() {
     <li><a href="#">Сальники</a></li>
     <li><a href="#">Ремни</a></li>
     </ul>`
-}
-)
+    catalogContain1.classList.remove("chosen")
+    catalogContain3.classList.remove("chosen")
+    catalogContain4.classList.remove("chosen")
+    catalogContain5.classList.remove("chosen")
+    catalogContain6.classList.remove("chosen")
+    catalogContain2.classList.add("chosen")
+})
 
-catalogContain3.addEventListener("mouseover", function() { 
+catalogContain3.addEventListener("mouseover", function () {
     catalogContent.innerHTML = `<ul>
     <li><a href="#">Конфорки чугунные</a></li>
     <li><a href="#">Электроконфорки промышленные</a></li>
     </ul>`
-}
-)
+    catalogContain2.classList.remove("chosen")
+    catalogContain1.classList.remove("chosen")
+    catalogContain4.classList.remove("chosen")
+    catalogContain5.classList.remove("chosen")
+    catalogContain6.classList.remove("chosen")
+    catalogContain3.classList.add("chosen")
+})
 
-catalogContain4.addEventListener("mouseover", function() { 
+catalogContain4.addEventListener("mouseover", function () {
     catalogContent.innerHTML = `<ul>
     <li><a href="#">Газовые плиты</a></li>
     <li><a href="#">Электроплиты</a></li>
     </ul>`
-}
-)
+    catalogContain2.classList.remove("chosen")
+    catalogContain3.classList.remove("chosen")
+    catalogContain1.classList.remove("chosen")
+    catalogContain5.classList.remove("chosen")
+    catalogContain6.classList.remove("chosen")
+    catalogContain4.classList.add("chosen")
+})
 
-catalogContain5.addEventListener("mouseover", function() { 
+catalogContain5.addEventListener("mouseover", function () {
     catalogContent.innerHTML = `<ul>
     <li><a href="#">Переключатели</a></li>
     <li><a href="#">Термостаты</a></li>
@@ -71,15 +91,24 @@ catalogContain5.addEventListener("mouseover", function() {
     <li><a href="#">Магнетроны</a></li>
     <li><a href="#">Предохранительные клапаны</a></li>
     </ul>`
-}
-)
+    catalogContain2.classList.remove("chosen")
+    catalogContain3.classList.remove("chosen")
+    catalogContain4.classList.remove("chosen")
+    catalogContain1.classList.remove("chosen")
+    catalogContain6.classList.remove("chosen")
+    catalogContain5.classList.add("chosen")
+})
 
-catalogContain6.addEventListener("mouseover", function() { 
+catalogContain6.addEventListener("mouseover", function () {
     catalogContent.innerHTML = `<ul>
     <li><a href="#">Заглушки в ТЭН</a></li>
     <li><a href="#">Шнур сетевой</a></li>
     <li><a href="#">Защита водонагревателя</a></li>
     </ul>`
-}
-)
-
+    catalogContain2.classList.remove("chosen")
+    catalogContain3.classList.remove("chosen")
+    catalogContain4.classList.remove("chosen")
+    catalogContain5.classList.remove("chosen")
+    catalogContain1.classList.remove("chosen")
+    catalogContain6.classList.add("chosen")
+})
